44a67c70ee2766afecf8be2624664915b245dabb
usman siddiqi <usmansiddiqi2000@gmail.com>
2025-02-08 22:51:51 +0530

    added latestcollection ,productitem

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      <img src={assets.logo} className='w-36' alt="" />
+      <Link to='/'><img src={assets.logo} className='w-36' alt="" /></Link>
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
         <NavLink to='/' className='flex flex-col gap-1 items-center ' >
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../component/Hero'
+import LatestCollection from '../component/LatestCollection'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <LatestCollection/>
     </div>
   )
 }

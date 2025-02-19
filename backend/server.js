5b1ebf5e0c65db45ef6cd9c2dba3cf66d7c9926b
usman siddiqi <usmansiddiqi2000@gmail.com>
2025-02-19 16:00:16 +0530

    started backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+//  app config==============
+
+const  app = express()
+const port = process.env.PORT || 4000
+
+// middlewares ===============
+
+app.use(express.json())
+app.use(cors())
+
+
+// api endpoint=====================
+
+app.get('/',(req,res)=>{
+    res.send('API Working')
+})
+
+app.listen(port,()=>console.log('Server Started on PORT :' + port))

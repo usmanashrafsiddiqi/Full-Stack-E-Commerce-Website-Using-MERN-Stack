26cdb279f92ad02c26a3bec59fec819d9a03e201
usman siddiqi <usmansiddiqi2000@gmail.com>
2025-04-07 18:17:38 +0530

    admin auth

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -61,6 +61,7 @@ const addProduct = async (req, res) => {
       res.json({ success: true, message: "Product Added" })
 
    } catch (error) {
+      console.log(error)
       res.json({ success: false, message: error.message })
    }
 
@@ -71,6 +72,14 @@ const addProduct = async (req, res) => {
 
 const listProducts = async (req, res) => {
 
+
+   try {
+      const products= await productModel.find({});
+      res.json({success:true,products})
+   } catch (error) {
+      console.log(error)
+      res.json({ success: false, message: error.message })
+   }
 }
 
 
@@ -78,12 +87,33 @@ const listProducts = async (req, res) => {
 
 const removeProduct = async (req, res) => {
 
+try {
+   await productModel.findByIdAndDelete(req.body.id)
+   res.json({success:true,message:"product removed"})
+   
+} catch (error) {
+   console.log(error)
+   res.json({ success: false, message: error.message })
+}
+
+
 }
 
 
 //funtion for single product info
 
 const singleProduct = async (req, res) => {
+
+ try {
+
+   const {productId }= req.body
+   const product = await productModel.findById(productId)
+   res.json({success:true,product})
+   
+ } catch (error) {
+   console.log(error)
+   res.json({ success: false, message: error.message })
+ }
 
 }
 
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -109,8 +109,21 @@ const userRegister = async (req, res) => {
 }
 
 //Route for  admin Login 
-const adminLogin = async () => {
-
+const adminLogin = async (req,res) => {
+try {
+    const {email,password} = req.body
+    if (email === process.env.ADMIN_EMAIL && password===  process.env.ADMIN_PASSWORD) {
+        const token = jwt.sign(email+password,process.env.JWT_SECRET)
+        res.json({success:true,token})
+        
+    }else{
+        res.json({success:false,message:"inavalid credentials"})
+    }
+} catch (error) {
+    
+    console.log(error);
+    res.json({ success: false, message: error.message })
+}
 }
 
 export { userLogin, userRegister, adminLogin };
